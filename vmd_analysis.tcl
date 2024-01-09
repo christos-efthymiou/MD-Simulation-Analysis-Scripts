37f3fc15546e08cb31f51e9600efe2f5ab9f32d9
christos-efthymiou <97161093+christos-efthymiou@users.noreply.github.com>
2024-01-09 13:03:31 -0800

    Update vmd_analysis.tcl
    Added the ability to calculate RMSD, RMSF, and salt bridges of a simulation

--- vmd_analysis.tcl
+++ vmd_analysis.tcl
@@ -1,6 +1,69 @@
 #load the PSF file that was used for your simulation
-mol new SCR20_K1230R_Neg_Control_100ns_Test3_QwikMD.psf type psf
+mol new Positive_Control.psf type psf
 
 #load the DCD file that you want to analyze - step value determines which frames to take from the simulation 
 #a step value of 2 is the default, which means every other frame will be loaded 
 mol addfile MD.dcd type dcd first 0 last -1 step 2 waitfor all
+
+#RMSD calculation 
+
+# wrap the trajectory to avoid RMSD calculation errors
+pbc wrap -center com -centersel "protein" -compound residue -all
+
+# Align to first frame or the frame you want to compare the rest of the frames to
+set reference [atomselect top "protein and backbone" frame 0]
+		
+# the frame to be compared
+set compare [atomselect top "protein and backbone"]
+
+#get the number of frames of the trajectory 
+set num_steps [molinfo top get numframes]
+
+# get the correct num as the trajectory start from zero
+set outfile [open rmsd.dat w]
+
+for {set frame 0} {$frame < $num_steps} {incr frame} {
+		# get the correct frame
+		$compare frame $frame
+		# compute the 4*4 matrix transformation that takes one set of coordinates onto the other 
+		set trans_mat [measure fit $compare $reference]
+		
+		# do the alignment
+		$compare move $trans_mat
+		# compute the RMSD
+		set rmsd [measure rmsd $compare $reference ]
+		# print the RMSD
+   puts $outfile "$frame    $rmsd"
+}
+close $outfile
+
+# RMSF calculation
+
+set num [expr {$num_steps - 1}]
+
+set outfile [open rmsf.dat w]
+set sel [atomselect top " protein and name CA"]
+set rmsf [measure rmsf $sel first 0 last $num step 1]
+for {set i 0} {$i < [$sel num]} {incr i} {
+  puts $outfile "[expr {$i+1}] [lindex $rmsf $i]"
+} 
+close $outfile
+
+# Salt bridges
+
+#create folder to store all of the saltbridge calculation files 
+file mkdir saltbridges
+package require saltbr
+
+#calculate the saltbridges
+saltbr -sel [atomselect top protein] -log saltbridges.log -outdir ./saltbridges
+
+#####Options for salt bridge:
+
+# -upsel <yes|no> (update atom selections every frame? default: yes)
+# -frames <begin:end> or <begin:step:end> or all or now (default: all)
+# -ondist <cutoff distance between oxygen and nitrogen atoms> (default: 3.2)
+# -comdist <cutoff distance between centers of mass of side chains> (default: none)
+# -writefiles <yes|no> (default: yes)
+#- outdir <output directory> (default: current)
+# -log <log filename> (default: none)
